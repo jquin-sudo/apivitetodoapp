4fd5abfb5acac1d6104f5e63bd83f7157313a221
Jesus Q <jquintos@stu.southtexascollege.edu>
2021-12-06 06:27:23 -0600

    api vitetodo done

---
+++ src/components/List.vue
@@ -0,0 +1,38 @@
+<script setup>
+  import useTodos from '../composables/useTodos'
+
+  const { pending, completed, changeStatus } = useTodos()
+
+  defineProps({
+    isCompleted: {
+      default: false,
+      type: Boolean,
+    },
+  })
+</script>
+
+<template>
+  <div class="w-1/3">
+    <h3
+      class="text-2xl text-center bg-yellow-700"
+      :class="isCompleted ? 'text-white' : 'text-white'"
+    >
+      {{ isCompleted ? 'Completed' : 'Pending' }}
+    </h3>
+    <ul class="pt-8 space-y-4">
+      <li
+        v-for="todo in isCompleted ? completed : pending"
+        :key="todo.id"
+        @click="changeStatus(todo.id)"
+        :class="
+          isCompleted
+            ? 'text-black hover:bg-yellow-700'
+            : 'text-black hover:bg-yellow-700'
+        "
+        class="w-full px-4 py-2 font-bold text-center transition-colors  bg-yellow-300 rounded-lg  hover:cursor-pointer hover:text-white"
+      >
+        {{ todo.text }}
+      </li>
+    </ul>
+  </div>
+</template>
---
+++ src/composables/useTodos.js
@@ -0,0 +1,57 @@
+import { ref, computed } from "vue";
+import axios from "axios";
+
+const todos = ref([]);
+
+const api = axios.create({
+  baseURL: "https://api.jquin.website/api/todos",
+  params: {
+    username: import.meta.env.VITE_API_USERNAME,
+    password: import.meta.env.VITE_API_PASSWORD,
+  },
+});
+
+const useTodos = () => {
+  const getAll = async () => {
+    const { data } = await api.get();
+    todos.value = data;
+  };
+
+  const pending = computed(() => {
+    return todos.value.filter((todo) => !todo.completed);
+  });
+
+  const completed = computed(() => {
+    return todos.value.filter((todo) => todo.completed);
+  });
+
+  const addTodo = async (newTodo) => {
+    if (newTodo.trim()) {
+      await api.post("", {
+        text: newTodo,
+        completed: false,
+      });
+      await getAll();
+    }
+  };
+
+  const changeStatus = async (id) => {
+    const todo = todos.value.find((todo) => todo.id === id);
+    todo.completed = !todo.completed;
+    const { id: _id, ...todoToUpdate } = todo;
+    await api.put(`/${id}`, todoToUpdate);
+    await getAll();
+  };
+
+  getAll();
+
+  return {
+    todos,
+    pending,
+    completed,
+    addTodo,
+    changeStatus,
+  };
+};
+
+export default useTodos;
